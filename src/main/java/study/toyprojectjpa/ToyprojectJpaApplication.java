b2d6d1e17fa7b622464d1ed624874a220e75a9dd
= <aamoos@naver.com>
2022-07-13 18:27:24 +0900

    Initial commit

---
+++ src/main/java/study/toyprojectjpa/ToyprojectJpaApplication.java
@@ -0,0 +1,13 @@
+package study.toyprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprojectJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprojectJpaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/study/toyprojectjpa/controller/MainController.java
@@ -0,0 +1,15 @@
+package study.toyprojectjpa.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+
+        return "views/content.html";
+    }
+
+}
